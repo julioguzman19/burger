c02a71eec6aad1d984effba8f913b30dd550d223
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-18 14:11:04 -0600

    initializing router in burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,16 @@
+// Requiringnpm express to create a router & burger.js for database
+let express = require("express");
+let burger = require("../models/burger.js");
+let router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+let burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
