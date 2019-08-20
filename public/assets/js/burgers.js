08c799dae25a84f59abed00f6438a160856b43a3
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-08-19 21:14:23 -0600

    devoured and not devoured list success

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -35,21 +35,6 @@ $(function() {
 				location.reload();
 			}
 		);
-	});
- $(".devourBurgerButton").on("click", function(event) {
-    let id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  }); 
-
+	})
 
 });
